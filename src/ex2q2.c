c76b4b45cc4e09e3e8a9906a4e8efc0bae05e097
Bobbie Angelo Tipan <btipan@ualberta.ca>
2025-09-20 00:46:47 -0600

    calculations are complete, just need to figure out commas.

--- src/ex2q2.c
+++ src/ex2q2.c
@@ -1,12 +1,13 @@
-#include <stdio.h>>
-#include <math.h>>
+#include <stdio.h>
+#include <math.h>
 
 float hypotenuse_calc(float x, float y)
 {
     float x_square, y_square, hypotenuse;
     x_square = x * x;
     y_square = y * y;
-    hypotenuse = sqrtf(x_square + y_square);
+    float length = x_square + y_square;
+    hypotenuse = sqrtf(length);
     return hypotenuse;
 }
 
@@ -23,49 +24,97 @@ float comparison(float length_one, float length_two, float length_three)
 {
     float smallest;
     if (length_one > length_two)
-        smallest = length_two;
-    else
-    smallest = length_one;
-    
-    if (length_one > length_three)
-        smallest = length_three;
-    else
-        smallest = length_one;
-    
-    if (length_two > length_three)
-        smallest = length_two;
-    else
-        smallest = length_three;
-    
+        if (length_two > length_three)
+        {
+            smallest = 3;
+        }
+        else
+        {
+            smallest = 2;
+        }
+    else if (length_two > length_one)
+    {
+        if (length_one > length_three)
+        {
+            smallest = 3;
+        }
+        else
+        {
+            smallest = 1;
+        }
+    }
+
     return smallest;
 }
 
 
-main(void)
+int main(void)
 {
     float x_1, y_1, x_2, y_2, x_3, y_3, distance_1, distance_2, distance_3, shortest;
+    int c_one, c_two, c_three;
 
     printf("Point 1: ");
-    scanf("(%f, %f)", &x_1, y_1);
-    printf("Point 1: ");
-    scanf("(%f, %f)", &x_2, y_2);
-    printf("Point 1: ");
-    scanf("(%f, %f)", &x_3, y_3);
+    c_one = scanf(" (%f, %f)", &x_1, &y_1);
+
+    printf("Point 2: ");
+    c_two = scanf(" (%f, %f)", &x_2, &y_2);
+
+    printf("Point 3: ");
+    c_three = scanf(" (%f, %f)", &x_3, &y_3);
+    printf("\n");
+
+    if (c_one != 2 || c_two != 2 || c_three != 2)
+    {
+        return -1;
+    }
 
     distance_1 = vector(x_1, y_1, x_2, y_2);
     distance_2 = vector(x_1, y_1, x_3, y_3);
     distance_3 = vector(x_2, y_2, x_3, y_3);
 
-    if (distance_1 == distance_2 && distance_3 == distance_2)
+    int check_dist1, check_dist2, check_dist3;
+    
+    check_dist1 = distance_1;
+    check_dist2 = distance_2;
+    check_dist3 = distance_3;
+
+    if (distance > 1000)
+    {
+        
+    }
+
+    if (check_dist1 == check_dist2 && check_dist3 == check_dist2)
     {
         printf("All points are equal distance: %.2f\n", distance_1);
         return 0;
     }
 
-    else
+    else if (check_dist1 == check_dist2)
     {
-        shortest = comparison()
+        printf("P1<->P2: %1.2f\nP1<->P3: %1.2f\n", distance_1, distance_2);
     }
 
+    else if (check_dist3 == check_dist2)
+    {
+        printf("P1<->P3: %1.2f\nP2<->P3: %1.2f\n", distance_2, distance_3);
+    }
+
+    else if (check_dist3 == check_dist1)
+    {
+        printf("P1<->P2: %1.2f\nP2<->P3: %1.2f\n", distance_1, distance_3);
+    }
+
+    else
+    {
+        shortest = comparison(distance_1, distance_2, distance_3);
+        if (shortest == 1)
+        printf("P1<->P2: %1.2f\n", distance_1);
+
+        else if (shortest == 2)
+        printf("P1<->P3: %1.2f\n", distance_2);
+
+        else
+        printf("P2<->P3: %1.2f\n", distance_3);
+    }
     return 0;
 }
